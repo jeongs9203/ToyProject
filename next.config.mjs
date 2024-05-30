be8eab4d467f020e205fdde05de141f9b1892e32
SO <so@Macui-MacBookAir.local>
2024-05-30 09:37:20 +0900

    init: first setting

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {},
+  images: {
+    domains: [],
+  },
+  // strict mode 끄기
+  reactStrictMode: false,
+};
+
+export default nextConfig;
